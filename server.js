93026cb1b9f2946eaa491f133e00a98c2fd1a036
benjamin dange <benjamin.dange@gmail.com>
2020-04-25 21:52:43 +0200

    Fixing heroku h10

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
+// const express = require('express');
+// const connectDB = require('./config/db');
+// const path = require('path');
+
+// const app = express();
+
+// // connect db
+// connectDB();
+
+// // Middlewares
+// app.use(express.json({ extended: false }));
+
+// // app.get('/', (req, res) => res.send('API running'));
+
+// // Define Routes
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/posts', require('./routes/api/posts'));
+
+// // Production
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 
 const app = express();
 
-// connect db
+// Connect Database
 connectDB();
 
-// Middlewares
+// Init Middleware
 app.use(express.json({ extended: false }));
-
-// app.get('/', (req, res) => res.send('API running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -18,8 +48,9 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// Production
+// Serve static assets in productions
 if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
